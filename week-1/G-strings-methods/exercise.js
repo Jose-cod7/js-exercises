d661887c6be6b8811c47875ec1c9ef1e06867190
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-02 12:53:11 +0200

    G-strings-done

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
-
-console.log(message);
+let message1 = "My name is Pepe and my name is ";
+let name = "Pepe";
+let nameLength = name.length;
+let long = " characters long";
+let message = message1 + " " + nameLength + long;
+console.log(message.trim());
