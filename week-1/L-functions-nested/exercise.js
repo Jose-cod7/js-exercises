733c164550accd4771853a1f5fa94381007eab1d
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-02 16:10:52 +0200

    functions nested

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,37 @@
-var mentor1 = "Daniel";
+/* var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function gretting(a) {
+    return "Hello " + a;
+}
+
+let mentoruno = gretting(mentor1);
+let mentordos = gretting(mentor2);
+let mentortres = gretting(mentor3);
+let mentorcuatro = gretting(mentor4);
+let mentorcinco = gretting(mentor5);
+
+console.log(mentoruno.toUpperCase());
+
+console.log(mentordos.toUpperCase());
+console.log(mentortres.toUpperCase());
+console.log(mentorcuatro.toUpperCase());
+console.log(mentorcinco.toUpperCase()); */
+
+function percentage(a, b) {
+    return (a / (a + b)) * 100;
+}
+
+function createGreeting()
+let percentageStudents = percentage(8, 15);
+
+let percentageMentors = percentage(15, 8);
+
+//let message = "Percentage students: 
+
+console.log(percentageStudents);
+
+console.log(percentageMentors);
