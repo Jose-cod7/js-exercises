b548f4f792b5759b540d39246b8ad29e69567d59
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-02 14:19:30 +0200

    floats-done

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,10 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+let totalOfPersons = numberOfMentors + numberOfStudents;
+let studentsPorcentSum = (numberOfStudents / totalOfPersons) * 100;
+let mentorsPorcentSum = (numberOfMentors / totalOfPersons) * 100;
+let mentorsPorcent = Math.round(mentorsPorcentSum);
+let studentsPorcent = Math.round(studentsPorcentSum);
+
+console.log("Percentage students: " + studentsPorcent + "%");
+console.log("Percentage mentors: " + mentorsPorcent + "%");
