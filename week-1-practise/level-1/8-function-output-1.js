0a2857b731648b505a101717cbdd6d89768e3125
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-03 12:41:41 +0200

    8-9-10done

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -9,4 +9,4 @@ console.log(result);
 // - What does this program do? Generate a random number between 0 and 1 except 1 and multiplies per 10.
 // - How many parameters does the function take? one
 // - What is the function name? getNumber
-// - Where is the function called? with what parameters? In line 5, with out a parameter
+// - Where is the function called? with what parameters? In line 5, with out a parameter.
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -2,13 +2,13 @@ function s(w1, w2) {
     return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. concatenate the two words and log it.
+// - How many parameters does the function take? Take two parameters.
+// - What is the function name? the function name is s.
+// - Where is the function called? with what parameters? Line 5 with 'code' and 'your'
+// - What does the function return? concatenate the two parameters.
 // - How can this function be improved?
