c53d99fc9f31ed86c999d0e2f862f97e45ff7b6f
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-03 19:58:12 +0200

    g-strings

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,7 +1,8 @@
 var name = " Daniel  ";
-var message1 = "My name is";
-let nameLength = name.length;
+var message1 = "My name is ";
+
+let namet = name.trim();
 let long = " characters long ";
 let myName = " and my name is ";
-let message = message1 + name.trim + myName + nameLength + long;
+let message = message1 + namet + myName + namet.length + long;
 console.log(message);
