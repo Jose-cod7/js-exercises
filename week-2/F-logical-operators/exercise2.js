f5db4d0d4dc842515bbc0cd7ecbc04ba444a1bcc
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-08 13:17:25 +0200

    F-logical-done

--- week-2/F-logical-operators/exercise2.js
+++ week-2/F-logical-operators/exercise2.js
@@ -5,7 +5,34 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(number) {
+    if (number <= 0) {
+        return true;
+    } else if (number > 0) {
+        return false;
+    }
+}
+
+function isBetween5and10(number) {
+    if (number >= 0 && number <= 10) {
+        return true;
+    }
+    return false;
+}
+
+function isShortName(name) {
+    if (name.length > 0 && name.length <= 6) {
+        return true;
+    }
+    return false;
+}
+
+function startsWithD(name) {
+    if (name.startsWith("D")) {
+        return true;
+    }
+    return false;
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
