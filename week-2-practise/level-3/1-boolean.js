34c371654a4872db64fa9f8ad00714ac1bcdab43
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-13 16:48:40 +0200

    practise-done

--- week-2-practise/level-3/1-boolean.js
+++ week-2-practise/level-3/1-boolean.js
@@ -1,18 +1,20 @@
 // The code is valid but is misleading and could be improved
 // Refactor the code to make it better
-// What was wrong with the original code?
+// What was wrong with the original code? two things; the first was the sign of the comparision operator, must have 3 equal signs instread of two, the other one is the value of tue variable and theother is the value of the variable.
 // Leave comments above your changes to explain
+// I add a equal sign in the comparision operator.
 
-var isHappy = "false";
+var isHappy = false;
 
-if (isHappy == true) {
-  console.log("I am happy");
+if (isHappy === true) {
+    console.log("I am happy");
 } else {
-  console.log("I am not happy");
+    console.log("I am not happy");
 }
 
 /* 
   EXPECTED RESULT
+  
   ---------------
   I am not happy
 */
