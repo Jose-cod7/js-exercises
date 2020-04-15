e3348f9e4a533f3a6777fa9ae1718eadc7a1fcec
Jose-cod7 <josegarriaga7@gmail.com>
2020-04-15 12:58:15 +0200

    week2-done

--- week-2-practise/level-2/5-indexOf.js
+++ week-2-practise/level-2/5-indexOf.js
@@ -8,8 +8,9 @@ function remove(arr, valueToRemove) {
     let indexToRemove = arr.indexOf(valueToRemove);
     let arrayBefore = arr.slice(0, indexToRemove);
     let arrayAfter = arr.slice(indexToRemove + 1, arr.length);
+    //console.log({ indexToRemove });
     //console.log({ arrayBefore });
-    //console.log({ arrayAfter });
+    console.log({ arrayAfter });
     return arrayBefore.concat(arrayAfter);
     // complete this statement
 }
